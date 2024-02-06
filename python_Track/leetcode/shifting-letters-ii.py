0c8c5486f3ea44454288e25a6e4ce7f093aee069
Chala <88979923+chalaa@users.noreply.github.com>
2024-02-06 11:57:49 +0300

    Add solution for Shifting Letters II

--- python_Track/leetcode/shifting-letters-ii.py
+++ python_Track/leetcode/shifting-letters-ii.py
@@ -2,11 +2,12 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         dic = dict(zip(string.ascii_lowercase, range(0,26)))
         rev_dic = {v:k for k,v in dic.items()}
-        d = [0 for _ in range(len(s) + 1)]
+        d = [0 for _ in range(len(s))]
         for start, end, direction in shifts:
             dir = 1 if direction else -1
             d[start] += dir
-            d[end + 1] -= dir
+            if end < len(s)-1:
+                d[end + 1] -= dir
         ans=""
         for i in range(len(s)):
             if i != 0: 
