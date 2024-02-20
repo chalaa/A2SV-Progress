991ca363b7efa85bdc2ad359efd79d76ea585596
Chala <88979923+chalaa@users.noreply.github.com>
2024-02-20 21:58:12 +0300

    Add solution for Merge Two Sorted Lists

--- python_Track/leetcode/merge-two-sorted-lists.py
+++ python_Track/leetcode/merge-two-sorted-lists.py
@@ -9,33 +9,40 @@ class Solution:
             return list2
         if not list2:
             return list1
-        head = None
-        tail = None
-        while list1 and list2:
-            if list1.val < list2.val:
-                if not head:
-                    head = list1
-                    tail = head
-                else:
-                    tail.next = list1
-                    tail = tail.next
-                list1 = list1.next
-            else:
-                if not head:
-                    head = list2
-                    tail = head
-                else:
-                    tail.next = list2
-                    tail = tail.next
-                list2 = list2.next
-        while list1:
-            tail.next = list1
-            tail = tail.next
-            list1 = list1.next
-        while list2:
-            tail.next = list2
-            tail = tail.next
-            list2 = list2.next
-        
-        return head
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next,list2)
+            return list1
+        else:
+            list2.next =self.mergeTwoLists(list1,list2.next)
+            return list2
+        # head = None
+        # tail = None
+        # def merge(list1,list2):
+        #     nonlocal head
+        #     nonlocal tail
+        #     if list1.val < list2.val:
+        #         if not head:
+        #             head = list1
+        #             tail = head
+        #         else:
+        #             tail.next = list1
+        #             tail = tail.next
+        #     else:
+        #         if not head:
+        #             head = list2
+        #             tail = head
+        #         else:
+        #             tail.next = list2
+        #             tail = tail.next
+        # while list1 and list2:
+        #     merge(list1,list2)
+        #     if list1.val < list2.val:
+        #         list1=list1.next
+        #     else:
+        #         list2=list2.next
+        # if list1:
+        #     tail.next = list1
+        # if list2:
+        #     tail.next = list2
 
+        # return head
