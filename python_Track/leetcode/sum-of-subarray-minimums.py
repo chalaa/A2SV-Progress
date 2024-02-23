5d1b9d892c50f8b94b2da8a1749e60597e66e871
Chala <88979923+chalaa@users.noreply.github.com>
2024-02-23 09:23:32 +0300

    Add solution for Sum of Subarray Minimums

--- python_Track/leetcode/sum-of-subarray-minimums.py
+++ python_Track/leetcode/sum-of-subarray-minimums.py
@@ -3,19 +3,20 @@ class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         n = len(arr)
         left = [-1] * n 
-        right = [n] * n
         stack = []
 
-        for i, value in enumerate(arr):
-            while stack and arr[stack[-1]] >= value:  
-                stack.pop()  
+        ## find the index of the next smallest to the left of the array
+        for i in range(len(arr)):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
             if stack:
-                left[i] = stack[-1]  
-            stack.append(i) 
+                left[i] = stack[-1]
+            stack.append(i)
 
+        right = [n] * n
         stack = [] 
 
-        
+        ## find the index of the next smallest to the left of the array
         for i in range(n - 1, -1, -1):  
             while stack and arr[stack[-1]] > arr[i]: 
                 stack.pop()  
@@ -24,7 +25,12 @@ class Solution:
             stack.append(i) 
 
         mod = 10**9 + 7 
-
-        result = sum((i - left[i]) * (right[i] - i) * value for i, value in enumerate(arr)) % mod
+        ans =  0
+        ## find the product of of the subarray arr[i] 
+        ## is minimum (i - left[i]) * (right[i] - i) * arr[i]
+        for i in range(n):
+            ans += (i - left[i]) * (right[i] - i) * arr[i]
+            ans%=mod
+        # ans = sum((i - left[i]) * (right[i] - i) * value for i, value in enumerate(arr)) % mod
       
-        return result 
+        return ans 
